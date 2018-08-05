36abc59b099d849bbc75fea4b2ad35d0562117ba
ChangJoo Park <pcjpcj2@gmail.com>
2018-08-05 13:50:47 +0900

    Fetch Tasks from firestore by user id

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
+import { AngularFirestore } from '../../../node_modules/angularfire2/firestore';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  items: Observable<any[]>;
+  constructor(
+    public navCtrl: NavController,
+    public afAuth: AngularFireAuth,
+    public afStore: AngularFirestore
+  ) {
 
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad')
+    this.fetchItemsByUserId()
+  }
+
+  fetchItemsByUserId() {
+    try {
+      this.afAuth.authState
+        .subscribe(user =>
+          this.items = this.afStore
+            .collection('items', ref => ref.where('userId', '==', user.uid))
+            .valueChanges()
+        )
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  newTodo() {
+    console.log('hello')
+    this.navCtrl.push('TodoNewPage')
+  }
 }
