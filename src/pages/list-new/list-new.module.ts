173a99fe080ec6cfb734af2f66ab23ce1e68ea5a
ChangJoo Park <pcjpcj2@gmail.com>
2018-08-05 20:08:00 +0900

    Add List New and Edit page

---
+++ src/pages/list-new/list-new.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ListNewPage } from './list-new';
+
+@NgModule({
+  declarations: [
+    ListNewPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ListNewPage),
+  ],
+})
+export class ListNewPageModule {}
