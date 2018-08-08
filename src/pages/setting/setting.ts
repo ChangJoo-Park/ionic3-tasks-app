8ade7d066dd89f76664baca689dfa6516f0d2423
ChangJoo Park <pcjpcj2@gmail.com>
2018-08-09 00:06:17 +0900

    Add displayName setting

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { AlertController, IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+
 import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
 
 /**
@@ -17,17 +18,19 @@ import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
   templateUrl: 'setting.html',
 })
 export class SettingPage {
+  currentUser: firebase.User;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public afAuth: AngularFireAuth,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController
   ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingPage');
+    this.currentUser = this.afAuth.auth.currentUser
   }
 
   async signOut() {
@@ -40,5 +43,42 @@ export class SettingPage {
     } finally {
       loader.dismiss()
     }
+  }
+
+  updateDisplayName() {
+    const user = this.afAuth.auth.currentUser
+
+    const displayNameConfirm = this.alertCtrl.create({
+      title: '이름 변경',
+      inputs: [
+        { name: 'displayName', value: user['displayName'] || '' }
+      ],
+      buttons: [
+        {
+          text: '취소',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: '확인',
+          handler: data => {
+            user.updateProfile({
+              displayName: data['displayName'],
+              photoURL: user['photoURL']
+            }).then(_ => {
+              // TODO: Toast
+              console.log('success')
+            }).catch(_ => {
+              // TODO: Toast
+              console.error('error')
+            })
+          }
+        }
+      ]
+    })
+    displayNameConfirm.present();
+    console.log('display name');
   }
 }
