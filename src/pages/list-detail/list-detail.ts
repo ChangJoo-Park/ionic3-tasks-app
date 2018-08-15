61aa52a83eb7325023d680dd7b58b0e7552b905d
ChangJoo Park <pcjpcj2@gmail.com>
2018-08-15 21:47:33 +0900

    Add list detail page

--- src/pages/list-detail/list-detail.ts
+++ src/pages/list-detail/list-detail.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Task } from '../../models/Task';
 
 /**
  * Generated class for the ListDetailPage page.
@@ -8,18 +14,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+@IonicPage({
+  name: 'ListDetailPage'
+})
 @Component({
   selector: 'page-list-detail',
   templateUrl: 'list-detail.html',
 })
 export class ListDetailPage {
+  list: Object;
+  tasks: Observable<Object[]>;
+  newTaskTitle: string;
+  userId: string;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public afAuth: AngularFireAuth,
+    public afStore: AngularFirestore
+  ) {
+    this.list = this.navParams.get('list')
+    console.log('this.list => ', this.list)
+    if (this.list === undefined) {
+      console.log('this list is undefined')
+      this.navCtrl.popAll();
+    }
+    this.afAuth.authState.subscribe(user => {
+      if (user) this.userId = user.uid
+      const archiveCollection = this.afStore
+        .collection('tasks', ref => ref
+          .where('userId', '==', user.uid)
+          .where('listId', '==', this.list['$key'])
+          // TODO: If list not found -> archive
+          .where('done', '==', false)
+          .orderBy('createdAt', 'asc')
+        )
+      this.tasks = archiveCollection
+        .snapshotChanges()
+        .pipe(
+          map(
+            actions =>
+              actions.map(action => ({
+                $key: action.payload.doc.id, ...action.payload.doc.data()
+              }))
+          )
+        )
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListDetailPage');
   }
 
+  addNewTask() {
+    try {
+      const newTask = (new Task(this.newTaskTitle, '', this.userId, '')).toJSON()
+      newTask['listId'] = this.list['$key']
+      this.newTaskTitle = '';
+      window.blur();
+      this.afStore.collection('tasks')
+        .add(newTask)
+        .then(_ => { console.log('created'); });
+    } catch (error) {
+      console.error(error)
+    } finally {
+      console.log('ended')
+    }
+  }
 }
